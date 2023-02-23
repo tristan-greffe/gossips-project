b95c3be089f88ad5f7377f9db385622295ecbbc1
tristan <tristan.greffe@hotmail.com>
2023-02-23 20:58:08 +0100

    build: initilized router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Contact from '../views/Contact.vue'
+import Team from '../views/Team.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
 
 const routes = [
   {
@@ -8,11 +11,31 @@ const routes = [
     name: 'home',
     component: Home,
   },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact,
+  },
+  {
+    path: '/team',
+    name: 'team',
+    component: Team,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUp,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-  })
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
 
 export default router
