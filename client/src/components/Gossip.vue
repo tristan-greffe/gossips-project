f17c63267b5ac068478a1d4cca40eb17670fd543
tristan <tristan.greffe@hotmail.com>
2023-02-23 20:59:53 +0100

    feat: create gossip component

---
+++ client/src/components/Gossip.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='card'>
+    <div class='bg-image hover-overlay ripple' data-mdb-ripple-color='light'>
+      <img :src="gossip.image" style="width: 100%"/>
+      <a href=''>
+        <div class='mask' style='background-color: rgba(251, 251, 251, 0.15);'></div>
+      </a>
+    </div>
+    <div class='card-body'>
+      <h5 class='card-title'>{{ gossip.author }}</h5>
+      <p class='card-text'>{{ gossip.content }}</p>
+      <a href='#!' class='btn btn-primary'>Read</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'gossip-component',
+    props: {
+      gossip: Object,
+    },
+  };
+</script>
