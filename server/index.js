9aa43113a8f284d4281b518f3f8bfbfd34ce1b2e
tristan <tristan.greffe@hotmail.com>
2023-02-23 20:42:24 +0100

    build: setup routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
+import GossipRoutes from './routes/gossips.js'
 
 const app = express()
-dotenv.config()
+app.use(express.json())
+app.use(cors())
+app.use('/gossips', GossipRoutes)
 
+dotenv.config()
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT =  process.env.PORT || 5000
 
