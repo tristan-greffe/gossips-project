ac1046e470d70c2248390e366308b4f2c84bab0a
tristan <tristan.greffe@hotmail.com>
2023-02-24 18:49:43 +0100

    build: add post route to create gossip

--- server/routes/gossips.js
+++ server/routes/gossips.js
@@ -1,10 +1,10 @@
 import express from 'express'
 
-import { getGossips } from '../controllers/gossips.js'
+import { getGossips, createGossip } from '../controllers/gossips.js'
 
 const router = express.Router()
 
 router.get('/', getGossips)
-
+router.post('/', createGossip)
 
 export default router
